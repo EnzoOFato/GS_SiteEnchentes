d8e0253fdefa7265810d8070510d5a81c21cdba2
Enzo Fatobene <enzoamafatobene@gmail.com>
2025-06-05 21:05:56 -0300

    Feat: botoes-cores

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,12 +1,26 @@
-const menuButton = document.getElementById('menu-toggle');
-const menu = document.getElementById('mobile-menu');
+const menuButton = document.getElementById("menu-toggle")
+const menu = document.getElementById("mobile-menu")
+const body = document.querySelector("body")
 
-menuButton.addEventListener('click', (e) => {
-    menu.classList.toggle('active');
-});
+menuButton.addEventListener("click", () => {
+    menu.classList.toggle("active")
+})
 
-document.addEventListener('click', (e) => {
+document.addEventListener("click", (e) => {
     if (!menuButton.contains(e.target)) {
-        menu.classList.remove('active');
+        menu.classList.remove("active")
     }
-});
+})
+
+function mudaCor(btn){
+    cor = btn.value
+    if(cor === "branco" ){
+        body.style.backgroundColor = "white"
+    }
+    else if(cor === "creme"){
+        body.style.backgroundColor = "rgb(255, 245, 235)"
+    }
+    else if(cor === "azul"){
+        body.style.backgroundColor = "lightblue"
+    }
+}
